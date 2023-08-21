d3adce30de537e03971b3a58385d9901c82a65b2
Nicholas Caporusso <ncaporusso@gmail.com>
2023-08-21 16:24:28 -0400

    Added material from week 1

---
+++ 01/02-phpinfo.php
@@ -0,0 +1,5 @@
+<?php
+// This is a single-line comment
+phpinfo(); // This function will display information about the PHP environment (web server)
+
+?>
---
+++ 01/03-first-php-page.php
@@ -0,0 +1,13 @@
+<h1>This is html code</h1>
+<?php
+// You can have multiple statements within a PHP tag
+echo '<h2>This is a sentence written using PHP</h2>';
+echo 'This is another sentence written using PHP';
+?>
+
+<h1>This is html code</h1>
+<?php
+// You can have multiple PHP tags on a page
+echo '<h2>This is a sentence written using PHP</h2>';
+echo 'This is another sentence written using PHP';
+?>
